f410274a63527186dfdb326a4c57c80521c1d260
MSoleimanzadeh <mina.soleimanzadeh@gmail.com>
2023-06-29 21:25:50 +0330

    Add contact-me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './sections/intro/intro.component';
+import { AboutMeComponent } from './sections/intro/about-me/about-me.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ContactMeComponent } from './sections/intro/contact-me/contact-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent
+    IntroComponent,
+    AboutMeComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
