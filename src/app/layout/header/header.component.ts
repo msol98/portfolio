527997df54e641b2789f2650a3299ce95d5cf2e7
MSoleimanzadeh <mina.soleimanzadeh@gmail.com>
2023-07-28 20:21:09 +0330

    Add elements, customize scroll bar and header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Subscription, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  scroll$?: Subscription;
+  startPosition: number = 0;
 
-  ngOnInit(): void {
+  @ViewChild('header') header?: ElementRef<HTMLElement>;
+
+  ngOnInit() {
+    this.startPosition = window.scrollY;
+    this.scroll$ = fromEvent(window, 'scroll').subscribe(e => {
+      const currentPosition = window.scrollY;
+      if (this.startPosition <= currentPosition) {
+        this.header?.nativeElement.classList.remove('h-show');
+        this.header?.nativeElement.classList.add('h-hide');
+      } else {
+        this.header?.nativeElement.classList.remove('h-hide');
+        this.header?.nativeElement.classList.add('h-show');
+      }
+      this.startPosition = currentPosition
+    });
   }
-
 }
