c1f3ea5591a754edc7902d1fcdcd9bdb76a15103
MSoleimanzadeh <mina.soleimanzadeh@gmail.com>
2023-08-18 18:11:53 +0330

    Add animation to the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'potfolio';
+  userHasVisited: boolean = false;
+
+  observers: Array<IntersectionObserver> = [];
+  @ViewChildren('component') components?: QueryList<ElementRef<HTMLElement>>;
+
+  constructor() {
+    history.replaceState({}, document.title, ".");
+  }
+
+  setPresent() {
+    document.body.classList.add('present');
+  }
+
+  ngOnInit() {
+    if (document.hasFocus()) {
+      this.setPresent();
+    } else {
+      document.addEventListener("visibilitychange", () => {
+        if (!document.hidden && !this.userHasVisited) {
+          this.setPresent();
+          this.userHasVisited = true;
+        }
+      }, false);
+    }
+  }
+
+  ngAfterViewInit() {
+    this.components?.toArray().forEach((fakeComponent: ElementRef<HTMLElement>, index) => {
+      let component;
+      const targetId = fakeComponent.nativeElement.nextElementSibling?.id;
+      if (targetId) component = document.getElementById(targetId);
+      if (component) {
+        const observer = this.creatObserver(component);
+        observer.observe(component);
+      }
+    });
+  }
+
+  creatObserver(component: HTMLElement) {
+    return new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting || entry.intersectionRatio > 0) {
+          component.classList.add('goingUpEl');
+        }
+      });
+    }, { threshold: 0.7, }
+    );
+  }
 }
