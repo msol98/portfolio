6b20d2dbe51b95c720b5709b69c78784e872acad
MSoleimanzadeh <mina.soleimanzadeh@gmail.com>
2025-07-27 08:00:48 +0330

    Chnage theme and assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { EducationComponent } from './sections/education/education.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { MAT_RIPPLE_GLOBAL_OPTIONS } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,7 @@ import { MaterialModule } from './material.module';
     BrowserAnimationsModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [{ provide: MAT_RIPPLE_GLOBAL_OPTIONS, useValue: { disabled: true } }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
