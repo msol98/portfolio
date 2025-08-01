e30da90aa96b189606267e37fbcac21f191defa1
MSoleimanzadeh <mina.soleimanzadeh@gmail.com>
2025-08-01 22:57:18 +0330

    Fix edits

--- src/app/sections/about-me/about-me.component.ts
+++ src/app/sections/about-me/about-me.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent  {}
+export class AboutMeComponent {
+  skills: string[] = ['JavaScript', 'Angular', 'React', 'TypeScript', 'HTML5', 'CSS3', 'Sass', 'Angular Material', 'Bootstrap', 'Git', 'Tailwind CSS', 'Redux', 'Zustand', 'Jest', 'GitHub', 'Figma'];
+}
