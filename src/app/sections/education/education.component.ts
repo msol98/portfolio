9c67fba5e912d9c431e1ae2d60cf0b70dce2b7a8
MSoleimanzadeh <mina.soleimanzadeh@gmail.com>
2025-09-10 16:46:10 +0330

    Redesign education component

--- src/app/sections/education/education.component.ts
+++ src/app/sections/education/education.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CertificateModel } from 'src/app/models/models';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.scss']
 })
-export class EducationComponent implements OnInit {
+export class EducationComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  certificates: Array<CertificateModel> = [
+    {
+      title: 'Electrical Engineering BSc.',
+      source: 'Iran University of Science and Technology',
+      date: '2016 - 2021',
+      imageUrl: 'assets/images/iust.webp'
+    },
+    {
+      title: 'Interactivity with JavaScript',
+      source: 'University of Michigan (Coursera)',
+      date: 'July 2020',
+      imageUrl: 'assets/images/interactivity-js.jpeg',
+      link: 'https://www.coursera.org/account/accomplishments/verify/7KM5VMQTH9D9'
+    },
+    {
+      title: 'Introduction to HTML5',
+      source: 'University of Michigan (Coursera)',
+      date: 'July 2020',
+      imageUrl: 'assets/images/introduction-html.jpeg',
+      link: 'https://www.coursera.org/account/accomplishments/verify/67L839VPYNKT'
+    }
+  ]
 
 }
